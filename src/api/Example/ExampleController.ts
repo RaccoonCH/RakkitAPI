41b3174b931f1d857f3cc4aa8d875281b8a88467
Owen Gombas <owen.gombas@gmail.com>
2018-10-17 16:36:26 +0200

    Code base improvements

--- src/api/Example/ExampleController.ts
+++ src/api/Example/ExampleController.ts
@@ -4,32 +4,30 @@ import { Query, Resolver, FieldResolver, Root } from 'type-graphql'
 @Resolver(ExampleModel)
 export default class ExampleController {
   @Query(returns => [ExampleModel])
-  getAllExample() {
+  async getAllExample() {
     return ExampleModel.find()
   }
 
-  // Add a computed field
-  // Root specify that the val paramater is the current object
   @FieldResolver()
   nameToUppercase(@Root() exampleInstance: ExampleModel): string {
-    return exampleInstance.name.toUpperCase()
+    return exampleInstance.name.toLocaleUpperCase()
   }
-
-  static async getOne(req, res) {
+  
+  static async getOne (req, res) {
     res.send(await ExampleModel.findOne(req.params.id))
     // findOne({id: req.params.id}) works too
   }
 
-  static async getAll(req, res) {
+  static async getAll (req, res) {
     res.send(await ExampleModel.find())
   }
 
-  static async create(req, res) {
+  static async create (req, res) {
     const m = new ExampleModel(req.body.name, req.body.text)
     res.send(await m.save())
   }
 
-  static async update(req, res) {
+  static async update (req, res) {
     const m = await ExampleModel.findOne(req.params.id)
     if (m) {
       m.name = req.body.name
@@ -40,7 +38,7 @@ export default class ExampleController {
     }
   }
 
-  static async remove(req, res) {
+  static async remove (req, res) {
     const m = await ExampleModel.findOne(req.params.id)
     if (m) {
       m.remove()
