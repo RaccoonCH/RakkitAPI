e16b0005db01b7e1e9a75942a71a6613e70476f3
Owen Gombas <owen.gombas@gmail.com>
2018-11-05 10:13:26 +0100

    User args

--- src/api/User/Types/UserArgs.ts
+++ src/api/User/Types/UserArgs.ts
@@ -3,12 +3,18 @@ import { QueryArgs } from '../../../class/App'
 import UserModel from '../UserModel'
 
 @InputType()
-export class UserArgs implements Pick<UserModel, 'Id' | 'Name'> {
+export class UserArgs implements Pick<UserModel, 'Id' | 'Name' | 'Email' | 'Role'> {
   @Field({ nullable: true })
   public readonly Id: number
 
   @Field({ nullable: true })
   public readonly Name: string
+
+  @Field({ nullable: true })
+  public readonly Email: string
+
+  @Field({ nullable: true })
+  public readonly Role: string
 }
 
 @ArgsType()
